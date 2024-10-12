5f798d9592c015a98b3f729cf561b39de314eb2a
Suyash Budhe <114820879+Suyash33@users.noreply.github.com>
2024-10-12 22:12:29 +0530

    Update AdminDashboard.js

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -10,7 +10,7 @@ const AdminDashboard = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/users'); 
+        const response = await axios.get('https://interntask-521i.onrender.com/api/users'); 
         setUsers(response.data);
       } catch (error) {
         console.error('Error fetching users:', error);
